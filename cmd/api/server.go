0c6d2df4b7829401f2aec81705be3b588fa168fa
Ashish Verma <ashv6561@gmail.com>
2025-06-19 16:20:38 +0530

    Added Basic server for School Admin application.

---
+++ cmd/api/server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Handle default route
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to ROOT Page...")
+	})
+
+	// Handle teachers route
+	http.HandleFunc("/teachers", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			fmt.Fprintf(w, "Welcome to Teachers Page... : GET Method")
+		case http.MethodPost:
+			fmt.Fprintf(w, "Welcome to Teachers Page... : POST Method")
+		case http.MethodPut:
+			fmt.Fprintf(w, "Welcome to Teachers Page... : PUT Method")
+		case http.MethodPatch:
+			fmt.Fprintf(w, "Welcome to Teachers Page... : PATCH Method")
+		case http.MethodDelete:
+			fmt.Fprintf(w, "Welcome to Teachers Page... : DELETE Method")
+		default:
+			fmt.Fprintf(w, "Invalid Request : %v", r.Method)
+		}
+	})
+
+	// Handle students route
+	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to Students Page...")
+	})
+
+	// Handle default route
+	http.HandleFunc("/execs", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to Executives Page...")
+	})
+
+	// Define Port and Start server
+	port := 3000
+
+	fmt.Println("Server running on Port :", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatalln("Couldn't start server... :", err)
+	}
+}
