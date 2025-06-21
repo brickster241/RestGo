d1e19ba43febbe369ea7a25d6c2e88f69307d28e
Ashish Verma <ashv6561@gmail.com>
2025-06-21 12:09:40 +0530

    Adding Middlewares for cors and security headers.

---
+++ internal/api/middlewares/cors.go
@@ -0,0 +1,43 @@
+package middlewares
+
+import "net/http"
+
+var allowedOrigins = []string {
+	"https://localhost:3000",
+	"https://my-origin-url.com",
+}
+
+func CorsMW(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+
+		// Only allow requests from specified urls' header.
+		if !isOriginAllowed(origin) {
+			http.Error(w, "Not Allowed by CORS.", http.StatusForbidden)
+			return
+		}
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+
+		// Handle PreFlight check
+		if r.Method == http.MethodOptions {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Simple Linear Search to check whether origin is present in the allowed list.
+func isOriginAllowed(origin string) bool {
+	for _, allowedOrigin := range allowedOrigins {
+		if origin == allowedOrigin {
+			return true
+		}
+	}
+	return false
+}
